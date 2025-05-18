6d0d00aa6ba74cf466c31ccbaa9f9d34cab1a13a
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-18 23:14:04 +0200

    Implemented editing and deleting dailyTask & connected it to the server (state management missing)

--- lib/widgets/daily_task_card.dart
+++ lib/widgets/daily_task_card.dart
@@ -25,11 +25,20 @@ class DailyTaskCard extends StatefulWidget {
 class _DailyTaskCardState extends State<DailyTaskCard> {
   late bool _isDone;
   bool _showActions = false;
+  bool _isEditing = false;
+  late TextEditingController _controller;
 
   @override
   void initState() {
     super.initState();
     _isDone = widget.isDone;
+    _controller = TextEditingController(text: widget.name);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   Future<void> _toggleIsDone() async {
@@ -56,6 +65,54 @@ class _DailyTaskCardState extends State<DailyTaskCard> {
     }
   }
 
+  Future<void> _editTask() async {
+    if (_isEditing) {
+      final newName = _controller.text.trim();
+
+      if (newName.isEmpty) return;
+
+      final url = Uri.parse(
+        'http://10.0.2.2:5000/api/v1/days/${widget.dayId}/daily-tasks/${widget.taskId}',
+      );
+
+      final response = await http.patch(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'name': newName}),
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          _isEditing = false;
+          _showActions = false;
+        });
+
+        widget.onUpdated();
+      } else {
+        debugPrint('Failed to edit daily task');
+      }
+    } else {
+      // Enter editting mode
+      setState(() {
+        _isEditing = true;
+      });
+    }
+  }
+
+  Future<void> _deleteTask() async {
+    final url = Uri.parse(
+      'http://10.0.2.2:5000/api/v1/days/${widget.dayId}/daily-tasks/${widget.taskId}',
+    );
+
+    final response = await http.delete(url);
+
+    if (response.statusCode == 200) {
+      widget.onUpdated();
+    } else {
+      debugPrint('Failed to delete daily task');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -67,8 +124,7 @@ class _DailyTaskCardState extends State<DailyTaskCard> {
         });
       },
       onTap: () {
-        // Tapping anywhere cancels action buttons
-        if (_showActions) {
+        if (_showActions && !_isEditing) {
           setState(() {
             _showActions = false;
           });
@@ -88,34 +144,42 @@ class _DailyTaskCardState extends State<DailyTaskCard> {
           children: [
             Checkbox(value: _isDone, onChanged: (_) => _toggleIsDone()),
             Expanded(
-              child: Text(
-                widget.name,
-                style: TextStyle(
-                  decoration: _isDone ? TextDecoration.lineThrough : null,
-                ),
-              ),
+              child:
+                  _isEditing
+                      ? TextField(
+                        style: TextStyle(fontSize: 14),
+                        controller: _controller,
+                        decoration: const InputDecoration(
+                          isDense: true,
+                          contentPadding: EdgeInsets.symmetric(vertical: 8),
+                          border: InputBorder.none,
+                        ),
+                        autofocus: true,
+                      )
+                      : Text(
+                        widget.name,
+                        style: TextStyle(
+                          decoration:
+                              _isDone ? TextDecoration.lineThrough : null,
+                        ),
+                      ),
             ),
             if (_showActions) ...[
               GestureDetector(
-                child: Container(
-                  padding: EdgeInsets.all(0),
-                  child: const Icon(Icons.edit, size: 18),
+                onTap: _editTask,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: Icon(_isEditing ? Icons.check : Icons.edit, size: 18),
                 ),
-                onTap: () {
-                  // TODO: Implement edit
-                },
               ),
-              SizedBox(width: 8),
               GestureDetector(
-                child: Container(
-                  padding: EdgeInsets.all(0),
-                  child: const Icon(Icons.delete, size: 18, color: Colors.red,),
+                onTap: _deleteTask,
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 4),
+                  child: Icon(Icons.delete, size: 18, color: Colors.red),
                 ),
-                onTap: () {
-                  // TODO: Implement delete
-                },
               ),
-              SizedBox(width: 8),
+              const SizedBox(width: 4),
             ],
           ],
         ),
